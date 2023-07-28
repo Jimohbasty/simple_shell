aa390484d374ae17240aaf44e988db3607f4ea83
Abdulbasit Jimoh <jimohabdulbasit@gmail.com>
2023-07-28 14:01:16 -0700

    new shell

--- _builtins_two.c
+++ _builtins_two.c
@@ -1,75 +1,22 @@
 #include "shell.h"
 
 /**
- * __alias - mimics the alias builtin (man alias)
+ * __history - displays the history list
+ * @info: info params from info struct
+ *  Return: 0 on success
  */
-int __alias(info_t *info)
+int __history(info_t *info)
 {
-	int i = 0;
-	char *p = NULL;
-	list_t *node = NULL;
-
-	if (info->argc == 1)
-	{
-		node = info->alias;
-		while (node)
-		{
-			print_list(node);
-			node = node->next;
-		}
-		return (0);
-	}
-	for (i = 1; info->argv[i]; i++)
-	{
-		p = _strchr(info->argv[i], '=');
-		if (p)
-			set_alias(info, info->argv[i]);
-		else
-			print_alias(node_starts_with(info->alias, info->argv[i], '='));
-	}
-
+	print_list(info->history);
 	return (0);
 }
 
 /**
- * set_alias - sets alias to string
- */
-int set_alias(info_t *info, char *str)
-{
-	char *p;
-
-	p = _strchr(str, '=');
-	if (!p)
-		return (1);
-	if (!*++p)
-		return (set_alias(info, str));
-
-	unset_alias(info, str);
-	return (add_node_end(&(info->alias), str, 0) == NULL);
-}
-
-/**
- * print_alias - prints an alias string
- */
-int print_alias(list_t *alias_node)
-{
-	char *p = NULL, *a = NULL;
-
-	if (alias_node)
-	{
-		p = _strchr(alias_node->str, '=');
-		for (a = alias_node->str; a <= p; a++)
-			_putchar(*a);
-		_putchar('\'');
-		_puts(p + 1);
-		_puts("'\n");
-		return (0);
-	}
-	return (1);
-}
-
-/**
  * unset_alias - un-sets alias to string
+ * @info: params from info struct
+ * @str: the string alias
+ * Return: Always 0 on success
+ * 1 on error
  */
 int unset_alias(info_t *info, char *str)
 {
@@ -89,10 +36,78 @@ int unset_alias(info_t *info, char *str)
 }
 
 /**
- * __history - displays the history list
+ * set_alias - sets alias to string
+ * @info: parameter struct
+ * @str: the string alias
+ * Return: Always 0 on success
+ * 1 on error
  */
-int __history(info_t *info)
+int set_alias(info_t *info, char *str)
 {
-	print_list(info->history);
+	char *p;
+
+	p = _strchr(str, '=');
+	if (!p)
+		return (1);
+	if (!*++p)
+		return (unset_alias(info, str));
+
+	unset_alias(info, str);
+	return (add_node_end(&(info->alias), str, 0) == NULL);
+}
+
+/**
+ * print_alias - prints an alias string
+ * @alias_node: the alias node
+ * Return: Always 0 on success
+ * 1 on error
+ */
+int print_alias(list_t *alias_node)
+{
+	char *p = NULL, *a = NULL;
+
+	if (alias_node)
+	{
+		p = _strchr(alias_node->str, '=');
+		for (a = alias_node->str; a <= p; a++)
+			_putchar(*a);
+		_putchar('\'');
+		_puts(p + 1);
+		_puts("'\n");
+		return (0);
+	}
+	return (1);
+}
+
+/**
+ * __alias - mimics the alias builtin (man alias)
+ * @info: info params from info struct
+ *  Return: Always 0
+ */
+int __alias(info_t *info)
+{
+	int i = 0;
+	char *p = NULL;
+	list_t *node = NULL;
+
+	if (info->argc == 1)
+	{
+		node = info->alias;
+		while (node)
+		{
+			print_alias(node);
+			node = node->next;
+		}
+		return (0);
+	}
+	for (i = 1; info->argv[i]; i++)
+	{
+		p = _strchr(info->argv[i], '=');
+		if (p)
+			set_alias(info, info->argv[i]);
+		else
+			print_alias(node_starts_with(info->alias, info->argv[i], '='));
+	}
+
 	return (0);
 }
